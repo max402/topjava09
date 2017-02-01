7311d29883e2b8daf6e2922ee9e7e9ce439fd15d
Grischenko Maxim <grischenko.maxim@gmail.com>
2017-02-01 17:06:22 +0200

    HW7 LocalDateTime converter (optional)

---
+++ src/main/java/ru/javawebinar/topjava/web/WebConfig.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+
+    }
+
+}
